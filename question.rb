03c8634ce097355a9d96293cf6c17e8abf63e44d
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-16 02:29:57 +0000

    Decide to create github repo for local project folder. Upload main.rb(empty), player.rb(basic class), question.rb(logic to generate a question), turn.rb(empty) and first draft of class design in .txt file.

---
+++ question.rb
@@ -0,0 +1,14 @@
+class Question
+
+  def self.get_question_answer 
+    output = []
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    output[0] = "What does #{@num1} plus #{@num2} equal?"
+    output[1] = [@num1 + @num2]
+    output
+  end
+
+end
+
+puts Question.get_question_answer
